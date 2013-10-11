951565e34ba91ae426930a8323486980bf740ff4
Stian <stian@stian.no>
2013-10-11 10:13:52 +0200

    reverted

--- TekanneMonoGame/TekanneMonoGame.cs
+++ TekanneMonoGame/TekanneMonoGame.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Tekanne;
 
 namespace TekanneMonoGame
 {
@@ -10,10 +9,9 @@ namespace TekanneMonoGame
         SpriteBatch _spriteBatch;
         Model teapot;
         Matrix projection;
+        Matrix view;
 
         float rotationX;
-
-        FlyingCamera camera = new FlyingCamera();
 
         public TekanneMonoGame()
         {
@@ -31,6 +29,7 @@ namespace TekanneMonoGame
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             teapot = Content.Load<Model>(@"teapot");
             projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver2, GraphicsDevice.Viewport.AspectRatio, 0.1f, 1000f);
+            view = Matrix.CreateLookAt(Vector3.Backward * 50f, Vector3.Zero, Vector3.Down);
         }
 
         protected override void UnloadContent()
@@ -40,8 +39,6 @@ namespace TekanneMonoGame
         protected override void Update(GameTime gameTime)
         {
             rotationX += (float)gameTime.ElapsedGameTime.TotalSeconds * MathHelper.TwoPi;
-
-            camera.Update((float)gameTime.ElapsedGameTime.TotalSeconds);
 
             base.Update(gameTime);
         }
@@ -56,14 +53,7 @@ namespace TekanneMonoGame
                 {
                     var effect = (meshPart.Effect as BasicEffect);
                     effect.Projection = projection;
-                    effect.View = camera.View;
-
-                    effect.EnableDefaultLighting();
-                    effect.DirectionalLight0.DiffuseColor = new Vector3(0.8f, 0.5f, 0.3f);
-                    effect.DirectionalLight0.Direction = new Vector3(0.5f, -0.5f, 0.5f);
-                    effect.DirectionalLight0.Enabled = true;
-
-                    effect.World =  Matrix.CreateRotationX(MathHelper.PiOver2 * 3) * Matrix.CreateRotationY(rotationX);
+                    effect.View = view;
                 }
 
                 mesh.Draw();
